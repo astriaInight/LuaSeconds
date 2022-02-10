c55fd995d4dbe6ae25f12ba9c0133e19cb00e10c
astrialnight <77846505+astriaInight@users.noreply.github.com>
2022-02-10 15:55:48 -0500

    Update seconds.lua

--- seconds.lua
+++ seconds.lua
@@ -2,26 +2,51 @@
 
 local Seconds = {}
 
+
 local worth = {
-	s  = 1,
-	m  = 60,
-	h  = 3600,
-	d  = 86400,
-	w  = 604800,
-	mo = 2629800,
-	y  = 31557600
+	{"y", 31557600},
+	{"mo", 2629800},
+	{"w", 604800},
+	{"d", 86400},
+	{"h", 3600},
+	{"m", 60},
+	{"s", 1}
 }
 
-function Seconds.ParseTimeString(timeStr: string): number
+
+function Seconds.StringToSeconds(timeStr: string): number
 	local totalSeconds: number = 0
 	
-	for sym, symWorth in pairs(worth) do
+	for _, time in pairs(worth) do
+		local sym = time[1]
+		local val = time[2]
+		
 		for num in string.gmatch(timeStr, "(%d+)"..sym) do
-			totalSeconds += symWorth * tonumber(num)
+			totalSeconds += val * tonumber(num)
 		end
 	end
 	
 	return totalSeconds
+end
+
+function Seconds.SecondsToString(seconds: number): string
+	local timeString: string = ""
+	
+	for _, time in pairs(worth) do
+		local sym = time[1]
+		local val = time[2]
+		
+		local div = seconds / val
+		
+		if div >= 1 then
+			timeString = timeString .. tostring(math.floor(div)) .. sym
+			
+			seconds -= math.floor(div) * val
+			print(seconds)
+		end
+	end
+	
+	return timeString
 end
 
 return Seconds
